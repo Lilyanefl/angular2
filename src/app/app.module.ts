a1f96346aa08fea3c43a001d5557aa028477da17
Melanie Santamaria <lilydtflowers@gmail.com>
2024-10-09 16:26:16 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Comp1Component } from './components/comp1/comp1.component';
+import { ActivePostComponent } from './components/active-post/active-post.component';
+import { InactivePostComponent } from './components/inactive-post/inactive-post.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    ActivePostComponent,
+    InactivePostComponent,
+    PostDetailComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/interfaces/interfaccia.ts
@@ -0,0 +1,2 @@
+export interface Interfaccia {
+}
